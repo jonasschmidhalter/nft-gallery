c487e5edbb7732fbbc7da86fc2080b33c612dfb8
jonasschmidhalter <jonas.schmidhalter@gmail.com>
2022-09-20 12:08:58 +0200

    INIT

---
+++ README.md
@@ -0,0 +1,59 @@
+# NFT Gallery Setup Guide
+
+## Requirements
+
+node - v16 or higher
+npm - v6 or higher
+
+Check with
+```sh
+node -v
+npm -v
+```
+
+## Setup
+
+### Installation
+
+Pull the project and run:
+```sh
+npm install
+```
+
+Compile the SCSS with:
+```sh
+npm run sass-dev
+```
+
+### Customization
+
+Customize Colors and Fonts in the following file: `/assets/scss/base/_variables.scss`
+
+For custom Fonts, add them to the file:  `/assets/scss/base/_fonts.scss` (best with an @import statement)
+
+### Contnet
+
+Add your content directly in the HTML file `index.html`
+
+To add new elements to your gallery, dubplicate the div with the class `galleryItem` and adjust the necessary information with your own information. The single view modal is automatically generated from the information you add in following divs:
+```sh
+.gallery__item-title        // Title of the NFT
+.gallery__item-author       // Author of the NFT
+.gallery__item-description  // Description of the NFT
+.gallery__item-link         // Link to the NFT
+
+.price                      // Price of the NFT
+.link-list                  // External Links to the NFT
+```
+
+### Videos / Images
+You can add a video (MP4) or an image to your NFT gallery. To do so, add the following divs to your `galleryItem`:
+```sh
+.gallery__item-video        // Video tag 
+.gallery__item-image        // Image
+```
+
+## Deploy to any hoster and enjoy
+
+
+
---
+++ assets/js/app.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+    let galleryItems = $('.galleryItem');
+
+    function timeout (ms) {
+        return new Promise(res => setTimeout(res,ms));
+    }
+    async function insertData(item) {
+        await clearModal();
+        let itemTitle, itemDescription, itemImage = "";
+        itemTitle = item.find('.gallery__item-title').html();
+        itemDescription = item.find('.gallery__item-description').html();
+        itemImage = item.find('.gallery__item-media img').attr('src');
+        if (itemImage === undefined) {
+            let itemVideo = item.find('video');
+            let videoPoster = itemVideo.attr('poster');
+            let videoSrc = itemVideo.find('source').attr('src');
+
+            let video = $('.modal__video');
+            video.find('source').attr("src", videoSrc);
+            video.attr("poster", videoPoster);
+            video.load();
+            $('.modal-media video').removeClass('d-none');
+            $('.modal__image').attr('src', '').addClass('d-none');
+        } else {
+            $('.modal__image').attr('src', itemImage).removeClass('d-none');
+            $('.modal-media video').addClass('d-none');
+        }
+        let itemLink = item.find('.gallery__item-link').attr('href');
+        let itemMeta = item.find('.gallery__item-meta').html();
+        $('.modal__title').html(itemTitle);
+        $('.modal__description').html(itemDescription);
+        $('.modal__link').attr('href', itemLink);
+        $('.modal__meta').html(itemMeta);
+    }
+    async function clearModal() {
+        $('.modal-media video').addClass('d-none');
+        $('.modal__image').attr('src', '').addClass('d-none');
+        $('.modal__title').html('');
+        $('.modal__description').html('');
+        $('.modal__link').attr('href', '');
+        $('.modal__meta').html('');
+    }
+    function getNextItem() {
+        let nextItem = $('.galleryItem.active').next();
+
+        if(nextItem.length === 0) {
+            nextItem = galleryItems.first();
+        }
+        galleryItems.removeClass('active');
+        nextItem.addClass('active');
+        insertData(nextItem);
+    }
+    function getPrevItem() {
+        let nextItem = $('.galleryItem.active').prev();
+
+        if(nextItem.length === 0) {
+            nextItem = galleryItems.last();
+        }
+        galleryItems.removeClass('active');
+        nextItem.addClass('active');
+        insertData(nextItem);
+    }
+    $('.modal-controls__next').on('click', getNextItem);
+    $('.modal-controls__prev').on('click', getPrevItem);
+
+    galleryItems.each(function() {
+        $(this).on('click', () => {
+            $(this).addClass('active');
+            $('#singleItemModal').modal('show');
+            insertData($(this));
+        });
+    })
+
+    $('.modal').on('swipeleft',function(event){
+        event.stopImmediatePropagation();
+        getNextItem();
+    }).on('swiperight',function(event){
+        event.stopImmediatePropagation();
+        getPrevItem();
+    });
+});
